a53aafd3e4a0ee57c943361ec5c371b9381034e1
nishanthan-k <krn.nishanthan@gmail.com>
2024-05-29 06:27:12 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="flex justify-between">
+      <div>Furnit.</div>
+      <div>Options</div>
+      <div>Icons</div>
+    </header>
+  );
+};
+
+export default Header;
